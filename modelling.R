b3e013d5912f5949cc785b94404b6ef52bf18b82
akhatre <akhatre@users.noreply.github.com>
2018-01-29 17:57:35 +0000

    asd

--- modelling.R
+++ modelling.R
@@ -1,7 +1,7 @@
 #parsing data ----
 
 library(jsonlite)
-alldata <- fromJSON(txt="~/Downloads/resproj-27787-export (14).json") #worked on 3, 5, 9
+alldata <- fromJSON(txt="~/Downloads/resproj-27787-export (20).json") #worked on 3, 5, 9
 
 ratios_sinusoid <- ratios_block <- data.frame(asd = rep(0,99))
 predictions_block_df <- predictions_sinusoid_df <- locations_block_df <- locations_sinusoid_df <- data.frame(asd = rep(0,100))
@@ -115,26 +115,48 @@ summary(model2)
 #estimating learning rate from the model
 long$set_as_number[long$set == "block"] <- -1
 long$set_as_number[long$set == "sinusoid"] <- 1
-long$eta <- long$error * (0.735  + 0.006 * long$volatility - 0.081 * long$set_as_number + 0.004 * long$set_as_number * long$volatility)
+long$eta <- 0.735  + 0.006 * long$volatility - 0.081 * long$set_as_number + 0.004 * long$set_as_number * long$volatility
 
 ids <- unique(long$id)
 etas <- data.frame(long$eta[long$id == ids[1]])
+plot(1:100, etas[,1], type = "l", ylim = c(0.5,1))
 for (n in 2:length(ids)) {
   etas <-  data.frame(etas, long$eta[long$id == ids[n]])
+  lines(1:100, long$eta[long$id == ids[n]])
 }
 etas <- rowSums(etas)/length(names(etas))
 plot(1:100, etas, type = "l")
 
 #fitting bootstrap filter ----
-ll_pf_bootstrap <- function(par, y, r, num_of_particles = 1000) { #that also should take y, r, and num_of_pacticles as input variables. Though, num_of_particles is not to be optimsed at the moment.
+library(truncnorm)
+resample_systematic <- function(weights) {
+  # input: weights is a vector of length N with (unnormalized) importance weights
+  # output: a vector of length N with indices of the replicated particles
+  N <- length(weights)
+  weights <- weights/sum(weights)# normalize weights
+  csum <- cumsum(weights)
+  u1 <- runif(1,min=0,max=1/N) # draw a single uniform number
+  u <- c(0, seq(1/N, (N-1)/N, length = N-1)) + u1
+  idx <- vector("integer",length=length(weights))
+  j <- 1
+  for(i in 1:N) {
+    while (u[i] > csum[j]) {
+      j <- j + 1
+    }
+    idx[i] <- j
+  }
+  return(idx)
+}
+ll_pf_bootstrap <- function(par, ta0, y, r, num_of_particles = 1000) { #that also should take y, r, and num_of_pacticles as input variables. Though, num_of_particles is not to be optimsed at the moment.
   # set parametrers
-  # num_of_particles = 1000 #that is just for testing, to be removed
-  # y <- long$locations[long$id == "3GeaVBWYDmYTCN1pa7OkP7gKwCO2"] #that is just for testing, to be removed
-  # r <- long$prediction[long$id == "3GeaVBWYDmYTCN1pa7OkP7gKwCO2"] #that is just for testing, to be removed
   
-  sd_ta = exp(par[1]) #sd of particle update distribution
-  sd_y = exp(par[2]) #noise of observations, real one was 4
-  sd_r = exp(par[3]) #noise of participants' responses, unknown
+  sd_y = exp(par[1]) #noise of observations, real one was 4
+  sd_r = exp(par[2]) #noise of participants' responses, unknown
+  if (ta0) {
+    sd_ta = 0
+  } else {
+    sd_ta = exp(par[3]) #sd of particle update distribution
+  }
   # Bootstrap filter
   # create matrices to store the particle values and weights
   n <- length(y)
@@ -191,200 +213,61 @@ ll_pf_bootstrap <- function(par, y, r, num_of_particles = 1000) { #that also sho
   mu <- pred
   # return(pred)
   
-  logLik <- -2*sum(dnorm(r,mean=mu,sd=sd_r,log=TRUE))	
+  logLik <- -2*sum(dnorm(r, mean=mu, sd=sd_r, log=TRUE))	
   return(logLik)
 }
 
+#serial optimisation
 estimated_par <- list()
-estimated_par <- optim(log(c(1,4,4)), ll_pf_bootstrap)
-
 for(i in unique(long$id)) {
   data <- subset(long, id==i)
   estimated_par[[i]] <- optim(log(c(1,4,4)), fn = ll_pf_bootstrap, y = data$locations, r = data$prediction)
 }
 
-prl <- function(i) {
+pars_btsrp_sd_ta_0 <- data.frame()
+for (i in names(estimated_par)) {
+  pars_btsrp_sd_ta_0 <- rbind(pars_btsrp_sd_ta_0, data.frame(noise = estimated_par[[i]]$par[1], sd_r = estimated_par[[i]]$par[2], logLik = estimated_par[[i]]$value))
+}
+summary(pars_btsrp_sd_ta_0$logLik)
+sd(pars_btsrp_sd_ta_0$logLik)
+
+estimated_par <- estimated_par_bootstrap_model
+pars_btsrp <- data.frame()
+for (i in names(estimated_par)) {
+  pars_btsrp <- rbind(pars_btsrp, data.frame(sd_ta = estimated_par[[i]]$par[1], noise = estimated_par[[i]]$par[2], sd_r = estimated_par[[i]]$par[3], logLik = estimated_par[[i]]$value))
+}
+summary(pars_btsrp$logLik[1:75])
+sd(pars_btsrp$logLik[1:75])
+
+#parallel optimisation
+library(parallel)
+prl_optimisation_btsrt_var_ta <- function(i) {
   data <- subset(long, id==i)
-  estimated_par[[i]] <- optim(log(c(1,4,4)), fn = ll_pf_bootstrap, y = data$locations, r = data$prediction)
+  est_par <- as.character(unique(data$id))
+  est_par <- c(est_par, optim(log(c(4,4,1)), method = "SANN", fn = ll_pf_bootstrap, ta0 = FALSE, y = data$locations, r = data$prediction))
+  return(est_par)
+}
+prl_optimisation_btsrt_fixed_ta <- function(i) {
+  data <- subset(long, id==i)
+  est_par <- as.character(unique(data$id))
+  est_par <- c(est_par, optim(log(c(4,4)), fn = ll_pf_bootstrap, ta0 = TRUE, y = data$locations, r = data$prediction))
+  return(est_par)
 }
 
-# Calculate the number of cores
 no_cores <- detectCores() - 1
+cluster <- makeCluster(no_cores, type = "FORK")
+params_btsrt_var_ta_sann <- parLapply(cluster, unique(long$id), fun = prl_optimisation_btsrt_var_ta)
+params_btsrt_fixed_ta <- parLapply(cluster, unique(long$id), fun = prl_optimisation_btsrt_fixed_ta)
+stopCluster(cluster)
 
-# Initiate cluster
-cl <- makeCluster(no_cores, type = "FORK")
-
-parLapply(cl, unique(long$id), fun = prl)
-
-stopCluster(cl)
-
-
-
-#filters (from ResProj_0.4, but now changed)
-resample_systematic <- function(weights) {
-  # input: weights is a vector of length N with (unnormalized) importance weights
-  # output: a vector of length N with indices of the replicated particles
-  N <- length(weights)
-  weights <- weights/sum(weights)# normalize weights
-  csum <- cumsum(weights)
-  u1 <- runif(1,min=0,max=1/N) # draw a single uniform number
-  u <- c(0, seq(1/N, (N-1)/N, length = N-1)) + u1
-  idx <- vector("integer",length=length(weights))
-  j <- 1
-  for(i in 1:N) {
-    while (u[i] > csum[j]) {
-      j <- j + 1
-    }
-    idx[i] <- j
-  }
-  return(idx)
+logLik_btsrt_fixed_ta <- logLik_btsrt_var_ta <- vector()
+for (i in 1:length(params_btsrt_fixed_ta)) {
+  logLik_btsrt_fixed_ta[i] <- as.numeric(params_btsrt_fixed_ta[[i]]$value)
+  logLik_btsrt_var_ta[i] <- as.numeric(params_btsrt_var_ta[[i]]$value)
 }
+summary(2*2 + logLik_btsrt_fixed_ta)
+sd(2*2 + logLik_btsrt_fixed_ta)
+summary(2*3 + logLik_btsrt_var_ta)
+sd(2*3 + logLik_btsrt_var_ta)
 
-pf_bootstrap <- function(y, num_of_particles) {
-  # Bootstrap filter
-  # create matrices to store the particle values and weights
-  n <- length(y)
-  logTa <- Ta <- Wa <- s <- k <- estim_state <- matrix(NA, ncol = num_of_particles, nrow = n) 
-  
-  # draw the particles for the initial state from the prior distribution
-  
-  logTa[1,] <- rnorm(num_of_particles, mean=log(5), sd=1)
-  # set Ta to e^logTa
-  Ta[1,] <- exp(logTa[1,])
-  
-  estim_state[1,] <- rep(0, num_of_particles)
-  k[1:2,] <- rep(0, num_of_particles)
-  s[1:2,] <- rep(1000, num_of_particles)
-  
-  Wa[1,] <- 1/num_of_particles
-  
-  # loop over time
-  for(t in 1:(n-1)) {
-    
-    # sample particles according to the transition distribution
-    
-    logTa[t+1,] <- rnorm(num_of_particles, mean=logTa[t,], sd = .1)
-    # set Ta to e^logTa
-    Ta[t+1,] <- exp(logTa[t+1,])
-    
-    k[t+1,] <- (s[t,] + Ta[t,]) / (s[t,] + Ta[t,] + 4^2)
-    s[t+1,] <- (1 - k[t+1,])*(s[t,] + Ta[t,])
-    estim_state[t+1,] <- estim_state[t,] + k[t+1,] * (y[t] - estim_state[t,])
-    
-    # compute the weights
-    Wa[t+1,] <- dnorm(y[t+1], mean = estim_state[t,], sd = sqrt(s[t+1,] + Ta[t+1,] + 4^2)) * Wa[t,] # could have multiplied by W, but not necessary with uniform weights
-    Wa[t+1,] <- Wa[t+1,]/sum(Wa[t+1,]) # normalize
-    
-    # draw indices of particles with systematic resampling
-    idx <- resample_systematic(Wa[t+1,])
-    
-    # implicitly W <- 1/num_of_particles
-    logTa[t+1,] <- logTa[t+1,idx]
-    Ta[t+1,] <- Ta[t+1,idx]
-    k[t+1,] <- k[t+1,idx]
-    s[t+1,] <- s[t+1,idx]
-    estim_state[t+1,] <- estim_state[t+1,idx]
-    
-    # reset the weights
-    Wa[t+1,] <- 1/num_of_particles
-  }
-  
-  return(rowSums(sqrt(Ta)*Wa))
-}
-
-
-model_tracking_volatility <- function(estim_noise, observations, num_of_particles) {
-  estim_vol <- pf_bootstrap(observations, num_of_particles)
-  eta <- 0
-  pred <- 0
-  n <- length(observations) - 1
-  
-  for (t in 1:n) {
-    eta[t+1] <- estim_vol[t+1]^2 / (estim_vol[t+1]^2 + estim_noise)
-    pred[t+1] <- pred[t] + eta[t+1] * (observations[t] - pred[t])
-  }
-  return(pred)
-}
-
-model_ignoring_volatility <- function(estim_noise, learning_rate, observations) {
-  eta <- learning_rate
-  pred <- 0
-  n <- length(observations) - 1
-  for (t in 1:n) {
-    pred[t+1] <- pred[t] + eta * (observations[t] - pred[t])
-  }
-  return(pred)
-}
-
-library(stats4)
-require(truncnorm)
-
-tvm_errors <- ivm_errors <- lrs <- array()
-a <- 1
-for (subject in names(locations_block_df)) {
-  # subject <- "YfKE878IgpYml15t6RlqJi8Hc9j1"
-  observations <- locations_block_df[[subject]]*300
-  subject_predictions <- predictions_block_df[[subject]]*300
-  tvm_predictions <- model_tracking_volatility(4, observations, 2000)
-  tvm_errors[a] <- sum((subject_predictions-tvm_predictions)^2)/100
-  
-  n <- 1
-  learning_rates <- ivm_to_data_error <- ivm_to_subject_error <- array()
-  for (i in seq(0, 1, 0.01)) {
-    ivm_predictions <- model_ignoring_volatility(4, i, observations)
-    learning_rates[n] <- i
-    ivm_to_data_error[n] <- sum((observations - ivm_predictions)^2)/100
-    ivm_to_subject_error[n] <- sum((subject_predictions - ivm_predictions)^2)/100
-    n = n + 1
-  }
-  
-  lrs[a] <- learning_rates[match(min(ivm_to_subject_error), ivm_to_subject_error)]
-  ivm_errors[a] <- min(ivm_to_subject_error)
-  
-  a <- a + 1
-}
-
-sum(ivm_errors)/length(ivm_errors)
-sum(tvm_errors)/length(tvm_errors)
-sum(lrs)/length(lrs)
-
-#ivm fitted to data
-learning_rate_to_data <- learning_rates[match(min(ivm_to_data_error), ivm_to_data_error)]
-ivm_predictions <- model_ignoring_volatility(4, learning_rate_to_data, observations)
-ivm_fitted_to_data_error <- sum((subject_predictions - ivm_predictions)^2)/100
-#ivm fitted to subject
-learning_rate_to_subject <- learning_rates[match(min(ivm_to_subject_error), ivm_to_subject_error)]
-ivm_predictions <- model_ignoring_volatility(4, learning_rate_to_subject, observations)
-ivm_fitted_to_subject_error <- sum((subject_predictions - ivm_predictions)^2)/100
-
-#checking
-tvm_error
-learning_rate_to_data
-ivm_fitted_to_data_error
-learning_rate_to_subject
-ivm_fitted_to_subject_error
-
-
-
-est_vol_bootstrap <- pf_bootstrap(y = as.numeric(locations)*300, num_of_particles = 2000)
-plot(1:100, est_vol_bootstrap, type = "l", ylab = "est vol bs", xlab = "Day (t)")
-
-pred_tv <- participant_tracking_volatility(estim_noise = 4, estim_vol = est_vol_bootstrap, observations = as.numeric(locations))
-
-mse_ignore_vol_model <- 0
-n <- 1
-for (i in seq(0, 1, 0.01)) {
-  pred_ignore_vol <- participant_ignoring_volatility(estim_noise = 4, learning_rate = i, observations = as.numeric(locations))
-  mse_ignore_vol_model[n] <- sum((as.numeric(locations) - pred_ignore_vol)^2)/100
-  n = n + 1
-}
-mse_ignore_vol_model <- min(mse_ignore_vol_model)
-
-pred_ntv <- participant_ignoring_volatility(estim_noise = 4, learning_rate = 0.5, observations = as.numeric(locations))
-
-mse_tv_vs_participant <- sum((as.numeric(predictions) - pred_tv)^2)/100
-mse_ntv_vs_participant <- sum((as.numeric(predictions) - pred_ntv)^2)/100
-
-mse_participant <- sum((as.numeric(locations) - as.numeric(predictions))^2)/100
-mse_tv_vs_data <- sum((as.numeric(locations) - pred_tv)^2)/100
-mse_ntv_vs_data <- sum((as.numeric(locations) - pred_ntv)^2)/100
+#filters (from ResProj_0.4, but now changed) ----
